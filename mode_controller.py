b209c71e6e51e4801728c754500b4c1c2eb7c809
Rajit <ri45@cornell.edu>
2015-12-14 16:57:02 -0500

    small change

--- mode_controller.py
+++ mode_controller.py
@@ -6,7 +6,7 @@ import sys
 depth = 6
 
 def human_vs_cpu(game):
-	game_tree = GameTree.GameTree(depth, game.board, game.turn, Heuristics.baseline)
+	game_tree = GameTree.GameTree(depth, game.board, game.turn)
 
 	# return the move the cpu player will make
 	return minimax_search.run_minimax(game_tree.root, depth, -sys.maxint - 1, sys.maxint)[1]
