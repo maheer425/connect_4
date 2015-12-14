a835f78f9f11d0e0b20ef3b3089828eac7e0a6b9
Rajit <ri45@cornell.edu>
2015-12-13 20:30:03 -0500

    Added heuristic call when constructing the tree

--- GameTree.py
+++ GameTree.py
@@ -1,5 +1,5 @@
 class GameTree:
-	def __init__(self,depth,board,turn):
+	def __init__(self,depth,board,turn,heuristicFunction):
 		self.root = TreeNode()
 		self.root.board = board
 		self.root.turn = turn
@@ -7,7 +7,7 @@ class GameTree:
 		self.createTree(0,self.root,depth)
 			
 
-	def createTree(self, curdepth,parent,maxdepth):
+	def createTree(self, curdepth,parent,maxdepth,heuristicFunction):
 		if(curdepth != maxdepth):
 			for j in range(0,7):
 				if checkLegal(parent.board,j):
@@ -22,23 +22,24 @@ class GameTree:
 						child.type = "min"
 					else:
 						child.type = "max"
+					child.board = parent.board
+					updateBoard(child,j)
 					if(curdepth == maxdepth-1):
 						child.isLeaf = True
-					child.board = parent.board
-					self.updateBoard(child,j)
+						child.heuristic = heuristicFunction(child.board,child.turn)
 					parent.children.append(child)
 					createTree(curdepth+1,child,maxdepth)
 
-	def checkLegal(board,move):
-		nextPos = ()
-		for y in range(0, 6):
-			if(board[move][y] == "empty"):
-				return True
-		return False
+def checkLegal(board,move):
+	nextPos = ()
+	for y in range(0, 6):
+		if(board[move][y] == "empty"):
+			return True
+	return False
 
-	def updateBoard(self, node, move):
-		nextPos = ()
-		for y in range(0, 6):
-			if(node.board[move][y] == "empty"):
-				nextPos = (move, y)
-		node.board[nextPos[0]][nextPos[1]] = node.turn
+def updateBoard(self, node, move):
+	nextPos = ()
+	for y in range(0, 6):
+		if(node.board[move][y] == "empty"):
+			nextPos = (move, y)
+	node.board[nextPos[0]][nextPos[1]] = node.turn
