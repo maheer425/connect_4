7d1dd8718e9692bf37d6cb4e9dcbd7c8d31f1668
Maheer Iqbal <mi236@cornell.edu>
2015-12-13 20:14:32 -0500

    modified minimax search with some additions to game tree

--- GameTree.py
+++ GameTree.py
@@ -4,10 +4,10 @@ class GameTree:
 		self.root.board = board
 		self.root.turn = turn
 		self.root.type = "max"
-		createTree(0,self.root,depth)
+		self.createTree(0,self.root,depth)
 			
 
-	def createTree(curdepth,parent,maxdepth):
+	def createTree(self, curdepth,parent,maxdepth):
 		if(curdepth != maxdepth):
 			for j in range(0,7):
 				if checkLegal(parent.board,j):
@@ -23,7 +23,7 @@ class GameTree:
 					else:
 						child.type = "max"
 					child.board = parent.board
-					updateBoard(child,j)
+					self.updateBoard(child,j)
 					parent.children.append(child)
 					createTree(curdepth+1,child,maxdepth)
 
@@ -34,7 +34,7 @@ class GameTree:
 				return True
 		return False
 
-	def updateBoard(node,move):
+	def updateBoard(self, node, move):
 		nextPos = ()
 		for y in range(0, 6):
 			if(node.board[move][y] == "empty"):
--- minimax_search.py
+++ minimax_search.py
@@ -5,28 +5,28 @@ def run_minimax (tree_node, d, minv, maxv):
 	a depth of d and returns the (score, move). move is in the range 
 	0..6 '''
 	if (d == 0 or tree_node.isLeaf = True):
-		return (tree_node.score, tree_node.move)
+		return (tree_node.heuristic, tree_node.lastMove)
 
-	if (tree_node.player == "max"):
+	if (tree_node.type == "max"):
 		node_value = minv
 		move_to_make = -1 # initially we don't know which move to make
 		for child_node in tree_node.children:
 			(child_node_value, child_move) = run_minimax(child_node, d-1, node_value, maxv)
 			if (child_node_value > node_value):
 				node_value = child_node_value 
-				move_to_make = child_node.move
+				move_to_make = child_node.lastMove
 			if (node_value > maxv): # same as min <= max
 				return (maxv, move_to_make)
 		return (node_value, move_to_make)
 
-	else: # player is min
+	else: # type is min
 		node_value = maxv
 		move_to_make = -1 # initially we don't know which move to make
 		for child_node in tree_node.children:
 			(child_node_value, child_move) = run_minimax(child_node, d-1, minv, node_value)
 			if (child_node_value < node_value):
 				node_value = child_node_value
-				move_to_make = child_node.move
+				move_to_make = child_node.lastMove
 			if (node_value < minv):
 				return (minv, move_to_make)
 		return (node_value, move_to_make)
