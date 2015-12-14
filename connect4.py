4e735175bdd4c424ea25cd17441bc16db09b521e
Maheer Iqbal <mi236@cornell.edu>
2015-12-13 19:52:18 -0500

    minimax with pruning untested

--- connect4.py
+++ connect4.py
@@ -16,7 +16,7 @@ class Connect4:
 				self.board[x][y-1+count] == self.turn and self.board[x][y+count] == self.turn):
 				print (self.turn + " won")
 				return True
-			elif(x-3+count >= 0 and y-3+count >= 0 and x+count <=6 and y+count <= 5 and self.board[x-3+count][y-3+count] == self.turn and self.board[x-2+count][y-2+count] == self.turn and #check diagonal win
+			elif(x-3+count >= 0 and 3-y+count >= 0 and x+count <=6 and y+count <= 5 and self.board[x-3+count][y-3+count] == self.turn and self.board[x-2+count][y-2+count] == self.turn and #check diagonal win
 				self.board[x-1+count][y-1+count] == self.turn and self.board[x+count][y+count] == self.turn):
 				print (self.turn + " won")
 				return True
@@ -43,6 +43,7 @@ class Connect4:
 			self.turn = "yellow"
 		else:
 			self.turn = "red"
+			
 
 	def endGame(self):
 		self.board =[["empty" for y in range(6)] for x in range(7)]
