e4549f126d0ae3edc3b8f6f4a8e8e9bc6cc53058
Rajit <ri45@cornell.edu>
2015-12-13 20:15:56 -0500

    added baseline heuristic

--- Heuristics.py
+++ Heuristics.py
@@ -1,2 +1,21 @@
-def threats(board,lastMove):
-	
+def baseline(board,turn):
+	score = 0
+	for x in range(0,7):
+		for y in range(0,6):
+			if(x>=1 and board[x-1][y] == turn and board[x][y] == turn):
+				score += 1
+			if(y>=1 and x>=1 and board[x-1][y-1] == turn and board[x][y] == turn):
+				score += 1
+			if(y>=1 and board[x][y-1] == turn and board[x][y] == turn):
+				score +=1
+			if(y>=1 and x<=6 and board[x+1][y-1] == turn and board[x][y] == turn):
+				score +=1
+			if(x<=6 and board[x+1][y] == turn and board[x][y] == turn):
+				score +=1
+			if(y<=5 and x<=6 and board[x+1][y+1] == turn and board[x][y] == turn):
+				score +=1
+			if(y<=5 and board[x][y+1] == turn and board[x][y] == turn):
+				score +=1
+			if(y<=5 and x>=1 and board[x-1][y+1] == turn and board[x][y] == turn):
+				score +=1
+	return score
