65683f59b2633e8a20328b6bbe31f94c6dff02df
Rajit <ri45@cornell.edu>
2015-12-14 21:56:52 -0500

    worked on threats heuristic

--- Heuristics.py
+++ Heuristics.py
@@ -22,20 +22,44 @@ def baseline(board,turn):
 
 def maximize_threats(board,turn):
 	winningRowsP1 = 0
+	score = 0
 	for x in range(0,7):
 		for y in range(0,6):
+			winningColor = horizontal_winning_row(board,turn,x,y)
+			if(turn == winningColor):
+				score += 1
+			elif(winningColor != "None"):
+				score -= 1
+			winningColor = vertical_winning_row(board,turn,x,y)
+			if(turn == winningColor):
+				score += 1
+			elif(winningColor != "None"):
+				score -= 1
+			winningColor = urdiagonal_winning_row(board,turn,x,y)
+			if(turn == winningColor):
+				score += 1
+			elif(winningColor != "None"):
+				score -= 1
+			winningColor = uldiagonal_winning_row(board,turn,x,y)
+			if(turn == winningColor):
+				score += 1
+			elif(winningColor != "None"):
+				score -= 1
+	return score
 
-def winning_row(board,turn):
+def horizontal_winning_row(board,turn,x,y):
 	found_color = False
 	found_enemy_color = False
 	if(turn == "red"):
 		enemyColor = "yellow"
 	else:
 		enemyColor = "red"
-	for x in range(0,7):
-		if(board[x,row] == turn):
+	for i in range(x,x+4):
+		if(i>=7):
+			return "None"
+		if(board[i][y] == turn):
 			found_color = True
-		if(board[x,row] == enemyColor):
+		if(board[i][y] == enemyColor):
 			found_enemy_color = True
 		if(found_color and found_enemy_color):
 			return "None"
@@ -46,6 +70,80 @@ def winning_row(board,turn):
 	else:
 		return "None"
 
+def vertical_winning_row(board,turn,x,y):
+	found_color = False
+	found_enemy_color = False
+	if(turn == "red"):
+		enemyColor = "yellow"
+	else:
+		enemyColor = "red"
+	for i in range(y,y+4):
+		if(i>=6):
+			return "None"
+		if(board[x][i] == turn):
+			found_color = True
+		if(board[x][i] == enemyColor):
+			found_enemy_color = True
+		if(found_color and found_enemy_color):
+			return "None"
+	if(found_color):
+		return turn
+	elif(found_enemy_color):
+		return enemyColor
+	else:
+		return "None"
+
+def urdiagonal_winning_row(board,turn,x,y):
+	found_color = False
+	found_enemy_color = False
+	if(turn == "red"):
+		enemyColor = "yellow"
+	else:
+		enemyColor = "red"
+	j = y
+	for i in range(x,x+4):
+		if(i>=7 or j >=6):
+			return "None"
+		if(board[i][j] == turn):
+			found_color = True
+		if(board[i][j] == enemyColor):
+			found_enemy_color = True
+		if(found_color and found_enemy_color):
+			return "None"
+		j+=1
+	if(found_color):
+		return turn
+	elif(found_enemy_color):
+		return enemyColor
+	else:
+		return "None"
+
+def uldiagonal_winning_row(board,turn,x,y):
+	found_color = False
+	found_enemy_color = False
+	if(turn == "red"):
+		enemyColor = "yellow"
+	else:
+		enemyColor = "red"
+	j = y
+	for i in range(x,x-4,-1):
+		if(i<=0 or j >=6):
+			return "None"
+		if(board[i][j] == turn):
+			found_color = True
+		if(board[i][j] == enemyColor):
+			found_enemy_color = True
+		if(found_color and found_enemy_color):
+			return "None"
+		j+=1
+	if(found_color):
+		return turn
+	elif(found_enemy_color):
+		return enemyColor
+	else:
+		
+		return "None"
+'''
 def streaks_value(board, turn):
 		""" Simple heuristic to evaluate board configurations
 			Heuristic is (num of 4-in-a-rows)*99999 + (num of 3-in-a-rows)*100 + 
@@ -148,3 +246,4 @@ def diagonalCheck(col, row, board, streak):
 		total += 1
 
 	return total
+	'''
