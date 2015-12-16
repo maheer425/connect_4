9dc0de22c641bbc88ea95c37b178a9c03dde0ca5
Maheer Iqbal <mi236@cornell.edu>
2015-12-16 16:54:12 -0500

    modified leaf allocation for streak boards

--- GameTree.py
+++ GameTree.py
@@ -2,11 +2,26 @@ import TreeNode
 import copy
 import Heuristics
 
-def checkLegal(board,move):
+def isLegal(board,move):
 	for y in range(0, 6):
 		if(board[move][y] == "empty"):
-			if(Heuristics.checkForStreak(board,"red",4) < 1 and Heuristics.checkForStreak(board,"yellow",4) < 1):
 				return True
+	return False
+
+def streak4OrDraw(board, move):
+
+	if(Heuristics.checkForStreak(board,"red", 4) > 0 or Heuristics.checkForStreak(board,"yellow", 4) > 0):
+		return True
+
+	chip_count = 0
+	for y in range(0, 6):
+		for x in range (0, 7):
+			if(board[x][y] != "empty"):
+				chip_count += 1
+
+	if (chip_count == 42):
+		return True
+
 	return False
 
 def updateBoard(node, move):
@@ -27,24 +42,33 @@ class GameTree:
 			
 
 
-	def createTree(self, curdepth,parent,maxdepth):
-		if(curdepth != maxdepth):
+	def createTree(self, curdepth, parent, maxdepth):
+		if((curdepth != maxdepth) and (not parent.isLeaf)):
 			for j in range(0,7):
-				if checkLegal(parent.board,j):
+				if (isLegal(parent.board, j)):
 					child = TreeNode.TreeNode()
 					child.parent = parent
 					child.lastMove = j
+
 					if(parent.turn == "red"):
 						child.turn = "yellow"
 					else:
 						child.turn = "red"
+
 					if(parent.type == "max"):
 						child.type = "min"
 					else:
 						child.type = "max"
+
 					child.board = copy.deepcopy(parent.board)
+
 					updateBoard(child,j)
+
 					if(curdepth == maxdepth-1):
 						child.isLeaf = True
+
+					if (streak4OrDraw(child.board, j)):
+						child.isLeaf = True
+
 					parent.children.append(child)
 					self.createTree(curdepth+1, child, maxdepth)
--- minimax_search.py
+++ minimax_search.py
@@ -1,41 +1,35 @@
 
-def run_minimax (tree_node, d, minv, maxv, heuristic):
+def run_minimax (tree_node, minv, maxv, heuristic):
 	'''Runs a minimax search on the given minimax tree node down to 
 	a depth of d and returns the (score, move). move is in the range 
 	0..6 '''
-	if (d == 0 or tree_node.isLeaf == True):
+	if (tree_node.isLeaf == True):
 		return (heuristic(tree_node.board, tree_node.turn), tree_node.lastMove)
 
 	if (tree_node.type == "max"):
-		node_value = minv
-
 		updated_minv = minv # this will change as we get child values
 
 		move_to_make = -1 # initially we don't know which move to make
 		for child_node in tree_node.children:
-			(child_node_value, child_move) = run_minimax(child_node, d-1, updated_minv, maxv, heuristic)
+			(child_node_value, child_move) = run_minimax(child_node, updated_minv, maxv, heuristic)
 			if (child_node_value > updated_minv):
 				updated_minv = child_node_value
-				node_value = child_node_value 
 				move_to_make = child_node.lastMove
 
 			if (maxv <= updated_minv): 
-				return (node_value, move_to_make)
-		return (node_value, move_to_make)
+				return (updated_minv, move_to_make)
+		return (updated_minv, move_to_make)
 
 	else: # type is min
-		node_value = maxv
-
 		updated_maxv = maxv # this will change as we get child values
 
 		move_to_make = -1 # initially we don't know which move to make
 		for child_node in tree_node.children:
-			(child_node_value, child_move) = run_minimax(child_node, d-1, minv, updated_maxv, heuristic)
+			(child_node_value, child_move) = run_minimax(child_node, minv, updated_maxv, heuristic)
 			if (child_node_value < updated_maxv):
 				updated_maxv = child_node_value
-				node_value = child_node_value
 				move_to_make = child_node.lastMove
 
 			if (updated_maxv <= minv):
-				return (node_value, move_to_make)
-		return (node_value, move_to_make)
+				return (updated_maxv, move_to_make)
+		return (updated_maxv, move_to_make)
